530b8e9357357dd0bf9df8f5fe84bc85a78977a7
victorialei21 <58520925+victorialei21@users.noreply.github.com>
2020-08-20 17:20:58 -0500

    installed firebase

--- store/actions/moods.js
+++ store/actions/moods.js
@@ -1,5 +1,25 @@
 export const ADD_MOOD = 'ADD_MOOD';
 
 export const addMood = (value, time) => {
-	return { type: ADD_MOOD, value, time };
+	return async (dispatch) => {
+		const response = await fetch(
+			'https://kunde-app-7f2e6.firebaseio.com/moods.json',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					value,
+					time,
+				}),
+			}
+		);
+
+		const resData = await response.json();
+
+		console.log(resData);
+
+		dispatch({ type: ADD_MOOD, value, time });
+	};
 };
